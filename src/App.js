c01f09489fed3e6c6127b0acf2f9f187c8c7d34e
Thomas Walker <thomasnwalker@yahoo.com>
2022-08-01 18:45:31 -0700

    app container

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
   
-        <p>
-          Mindfulnest - positive affirmations 
+        <div className="app-control-container"> 
+          <p>
+         CONSOLE
         </p>
-
-        <div className="button-box">
-                     bbb
+      <div className="button-box">
+          buttons
+      </div>
         </div>
+
+        
       </header>
     </div>
   );
