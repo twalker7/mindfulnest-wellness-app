78e3e28eb0edebed47834d4af80c136f9567e0d1
Thomas Walker <thomasnwalker@yahoo.com>
2022-09-20 15:17:02 -0700

    testing a videocamera in audiorecord element (very cool and fun)

--- src/components/RecordComponent/index.js
+++ src/components/RecordComponent/index.js
@@ -7,15 +7,36 @@ function RecordComponent(props){
         alert("This button is under construction!")
     
       }
-    
-      
+
+
+    let constraintObj = { 
+        audio: false, 
+        video: { 
+            facingMode: "user", 
+            width: { min: 640, ideal: 1280, max: 1920 },
+            height: { min: 480, ideal: 720, max: 1080 } 
+        } 
+    }; 
+
+
+    navigator.mediaDevices.getUserMedia(constraintObj)
+    .then(function(mediaStreamObj) {
+        //connect the media stream to the first video element
+        let video = document.querySelector('video');
+        if ("srcObject" in video) {
+            video.srcObject = mediaStreamObj;
+        } else {
+            //old version
+            video.src = window.URL.createObjectURL(mediaStreamObj);
+        }
+      }   ); 
     return(
         <div className="record-component-container">
         <div className="button-box">
         
         <Button variant="light" onClick={playAudio}> Record  </Button>
   </div>
-        <audio id="player" controls></audio>
+        <video id="video" controls></video>
 
 
    
