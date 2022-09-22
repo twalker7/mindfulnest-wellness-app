029bd053c904e226d0d059709b101645fc5d7ab3
Thomas Walker <thomasnwalker@yahoo.com>
2022-09-22 16:12:47 -0700

    added recordability element --very rough ui

--- src/components/RecordComponent/index.js
+++ src/components/RecordComponent/index.js
@@ -30,21 +30,42 @@ function RecordComponent(props){
             video.src = window.URL.createObjectURL(mediaStreamObj);
         }
 
-        video.onloadedmetadata = function(ev) {
-          // to automatically show in the video element what is being captured by the webcam without pressing play button
-          video.play();
-        }
+                        
+   //add listeners for saving video/audio
+   let start = document.getElementById('btnStart');
+   let stop = document.getElementById('btnStop');
+   let vidSave = document.getElementById('vid2');
+   let mediaRecorder = new MediaRecorder(mediaStreamObj);
+   let chunks = [];
 
-                         //add listeners for saving video/audio
-                         let start = document.getElementById('btnStart');
-                         let stop = document.getElementById('btnStop');
-                         let vidSave = document.getElementById('vid2');
-                         let mediaRecorder = new MediaRecorder(mediaStreamObj);
-                         let chunks = [];
-             
-                         
-      }   ); 
 
+
+  start.addEventListener('click', (ev)=>{
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+})
+stop.addEventListener('click', (ev)=>{
+    mediaRecorder.stop();
+    console.log("mediaRecorder.state");
+});
+ mediaRecorder.ondataavailable = function(ev) {
+    chunks.push(ev.data);
+}
+
+mediaRecorder.onstop = (ev)=>{
+    let blob = new Blob(chunks, { 'type' : 'video/mp4;' });
+    chunks = [];
+    let videoURL = window.URL.createObjectURL(blob);
+    vidSave.src = videoURL;
+}
+
+})
+
+.catch(function(err) { 
+console.log(err.name, err.message); 
+}); 
+ 
+      
 
  
 
