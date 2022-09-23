c869e3d029e2d326aeab7a671892c57af0c1c700
Thomas Walker <thomasnwalker@yahoo.com>
2022-09-22 17:52:27 -0700

    styled about section a bit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About(props){
     return(
         <div className="about-component-container">
             About 
-            <div className="about-content-section"> Mindfulnest is an application created to promote and regulate mental and physical wellness . Affirmations and mantras have been used by many different cultures for many years as an understo </div>
+            <div className="about-content-section"> Mindfulnest is an application created to promote and regulate mental and physical wellness. Affirmations and mantras have been used by many different cultures for many years as technique to internally and externally improve one's conditions. Mindfulnest provides not only provides users with affirmations; it also gives you the chance to record and download your own! </div>
             <div className="back-button"> <a  onClick={()=> props.setCurrentComponent('home')}> back</a></div>
     
         </div>
