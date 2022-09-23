2833c257f63c0cd17027ccbdafa81263156dbaef
Thomas Walker <thomasnwalker@yahoo.com>
2022-09-23 09:57:46 -0700

    rearranged menu

--- src/components/HomeMenu/index.js
+++ src/components/HomeMenu/index.js
@@ -9,9 +9,10 @@ function HomeMenu(props){
         <div className="home-menu-container">
 
                 <div> <div className="home-menu-element"  onClick={()=> props.setCurrentComponent('audio')}>  Audio Tracks  </div></div>
+                <div> <div className="home-menu-element"  onClick={()=> props.setCurrentComponent('record')}> Record Yours</div></div>
                 <div> <div className="home-menu-element"  onClick={()=> props.setCurrentComponent('about')}> About </div> </div>
                 <div> <div className="home-menu-element"  onClick={()=> props.setCurrentComponent('contact')} > Contact </div> </div>
-                <div> <div className="home-menu-element"  onClick={()=> props.setCurrentComponent('record')}> Record and Play</div></div>
+
 
         </div>
     )
