bf4e40ae58e33f1ddd107f0a754fa18c8094a2fc
twalker7 <thomasnwalker@yahoo.com>
2022-11-06 22:14:01 -0800

    added funny picture for empty contact componeent

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,10 +4,11 @@ function Contact(props){
     return(
         <div className="contact-component-container">
             Contact Us 
-            <div className=""> 
+            <div className="contact-box"> 
             <h3>  </h3>
             <h3>  </h3>
-            <img src=""/>
+            
+         
             </div>
 
             <div className="back-button"> <a  onClick={()=> props.setCurrentComponent('home')}> back</a></div>
