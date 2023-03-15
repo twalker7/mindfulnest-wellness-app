2aa0cb18bd3dc123ac3af37c8ada9509b3f44ccd
twalker7 <thomasnwalker@yahoo.com>
2023-03-14 23:08:13 -0400

    inserted container bx for embedded video into about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,6 +5,7 @@ function About(props){
         <div className="about-component-container">
             About 
             <div className="about-content-section"> Mindfulnest is an application created to promote and regulate mental and physical wellness. Affirmations and mantras have been used by many different cultures for generations as a technique to improve one's internal and external conditions. Mindfulnest not only provides users with affirmations–– it also gives you the chance to record and download your own! </div>
+            <div className="about-section-video"> </div>
             <div className="back-button"> <a  onClick={()=> props.setCurrentComponent('home')}> back</a></div>
     
         </div>
